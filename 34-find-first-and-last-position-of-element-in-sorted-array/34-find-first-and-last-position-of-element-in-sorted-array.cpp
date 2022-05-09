6f23fec07ef011ef87bda34b39b4f132d820d008
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-09 12:42:35 +0530

    Time: 12 ms (47.27%), Space: 13.7 MB (65.35%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int a = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        
-        int b = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        
-        if(binary_search(nums.begin(),nums.end(),target)){
-            return {a,b-1};
+        int a = -1,b = -1;
+        int s = 0,e = nums.size()-1;
+        int mid = s+(e-s)/2;
+        while(s<=e){
+            mid = s+(e-s)/2;
+            if(nums[mid] == target){
+                a = mid;
+                e = mid-1;
+                continue;
+            }
+            else if(nums[mid]<target){
+                s = mid+1;
+            }
+            else if(nums[mid]>target){
+                e = mid-1;
+            }
         }
-        else{
-            return {-1,-1};
+        
+        int s1 = 0,e1 = nums.size()-1;
+        int mid1 = s1+(e1-s1)/2;
+        while(s1<=e1){
+            mid1 = s1+(e1-s1)/2;
+            if(nums[mid1] == target){
+                b = mid1;
+                s1 = mid1+1;
+                continue;
+            }
+            else if(nums[mid1]<target){
+                s1 = mid1+1;
+            }
+            else{
+                e1 = mid1-1;
+            }
         }
+        
+        return {a,b};
+        
+        
     }
 };
