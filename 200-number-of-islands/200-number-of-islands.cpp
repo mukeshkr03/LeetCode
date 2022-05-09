76075bd9f9ca81ea3b3ae9cbd112f6ac8befc3da
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-09 10:56:16 +0530

    Time: 38 ms (78.27%), Space: 12.3 MB (58.57%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -9,9 +9,9 @@ public:
    void dfs(vector<vector<char>>& grid,int i,int j,int m,int n){
         grid[i][j]='v';  // mark as visited
         
-        if(i<m-1 && grid[i+1][j]=='1') dfs(grid,i+1,j,m,n);
+        if(i<n-1 && grid[i+1][j]=='1') dfs(grid,i+1,j,m,n);
         if(i>0 && grid[i-1][j]=='1') dfs(grid,i-1,j,m,n);
-        if(j<n-1 && grid[i][j+1]=='1') dfs(grid,i,j+1,m,n);
+        if(j<m-1 && grid[i][j+1]=='1') dfs(grid,i,j+1,m,n);
         if(j>0 && grid[i][j-1]=='1') dfs(grid,i,j-1,m,n);
         else return;
     }
@@ -24,7 +24,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j,n,m);
+                    dfs(grid,i,j,m,n);
                     cnt++;
                 }
             }
