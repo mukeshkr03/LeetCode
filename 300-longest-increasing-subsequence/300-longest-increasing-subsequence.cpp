1924185c60185cf811585f374d2da09c121025e2
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-13 13:17:39 +0530

    Time: 334 ms (47.42%), Space: 10.4 MB (88.03%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -13,12 +13,16 @@ public:
         return dp[i] = sol;
     }
     int lengthOfLIS(vector<int>& nums) {
-       int dp[nums.size()];
-        memset(dp,-1,sizeof dp);
-        int ans = INT_MIN;
-        for(int i = 0;i<nums.size();i++){
-            ans = max(ans,lis(dp,i,nums));
+       int n = nums.size();
+        int dp[n];
+        for(int i = 0;i<n;i++){
+            dp[i] = 1;
+            for(int j = 0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                }
+            }
         }
-        return ans;
+        return *max_element(dp,dp+n);
     }
 };
