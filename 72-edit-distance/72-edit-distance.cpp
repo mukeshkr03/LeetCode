f67f59dc7682ec1d71fdaba8928003a6365c3d12
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-20 16:45:00 +0530

    Time: 9 ms (83.88%), Space: 7.4 MB (74.31%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length(),m = word2.length();
+        int dp[n+1][m+1];
+        
+        for(int j=0;j<=n;j++) dp[j][0] = j;
+        for(int i=0;i<=m;i++) dp[0][i] = i;
+        
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int a = 1 + dp[i-1][j-1];
+                    int b = 1 + dp[i-1][j];
+                    int c = 1 + dp[i][j-1];
+                    dp[i][j] = min({a,b,c});
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
