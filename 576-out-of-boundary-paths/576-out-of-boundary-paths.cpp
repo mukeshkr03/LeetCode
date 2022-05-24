36a92c9a55fa1b76316b5243f27eae3ae67aaf71
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-24 14:45:52 +0530

    Time: 13 ms (59.73%), Space: 9.6 MB (24.70%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int dp[51][51][51];
+    // int dp[51][51][51];
     int mod = 1e9 + 7;
     
-    long long solve(int m, int n, int maxMove, int i, int j){
+    long long solve(int m, int n, int maxMove, int i, int j ,vector<vector<vector<int>>>&dp){
         
         if(i<0 || j<0 || i>=m || j>=n){
             return 1;
@@ -17,16 +17,18 @@ public:
         }
         long long ans = 0;
         
-        ans+= solve(m,n,maxMove-1, i-1, j);
-        ans+= solve(m,n,maxMove-1, i, j-1);
-        ans+= solve(m,n,maxMove-1, i+1, j);
-        ans+= solve(m,n,maxMove-1, i, j+1);
+        ans+= solve(m,n,maxMove-1, i-1, j,dp);
+        ans+= solve(m,n,maxMove-1, i, j-1,dp);
+        ans+= solve(m,n,maxMove-1, i+1, j,dp);
+        ans+= solve(m,n,maxMove-1, i, j+1,dp);
         
         return dp[i][j][maxMove] = ans%mod;
     }
     
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        memset(dp,-1,sizeof dp);
-        return solve(m,n, maxMove, startRow, startColumn)%mod;
+        vector<vector<vector<int>>>dp(m+1, vector<vector<int>>(n+1,vector<int>(maxMove+1,-1)));
+        // vector<vector<vector<int>>>dp(maxMove+1, vector<vector<int>> (m+1, vector<int>(n+1,-1)));
+        // memset(dp,-1,sizeof dp);/
+        return solve(m,n, maxMove, startRow, startColumn , dp)%mod;
     }
 };
