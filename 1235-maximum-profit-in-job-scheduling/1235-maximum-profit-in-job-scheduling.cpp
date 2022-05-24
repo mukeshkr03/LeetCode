18ff65dc425c3865f45b21073edf2daa923dcd63
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-24 12:30:11 +0530

    Time: 1341 ms (5.03%), Space: 230.3 MB (5.03%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    static bool cmp(vector<int>v1, vector<int>v2){
+        return v1[1]<v2[1];
+    }
+        
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<vector<int>>v(n);
+        for(int i = 0;i<n;i++){
+            v[i] = {startTime[i],endTime[i],profit[i]};
+        }
+        sort(v.begin(),v.end(),cmp);
+        
+        int dp[n];
+        dp[0] = v[0][2];
+        for(int i = 1;i<n;i++){
+            int s = 0, e = i-1;
+            int inc = v[i][2];
+            int get = -1;
+            while(s<=e){
+                int mid = s + (e - s)/2;
+                if(v[mid][1]<=v[i][0]){
+                   s = mid + 1;
+                    get = mid;
+                }
+                else{
+                   e = mid - 1;
+                }
+            }
+            if(get != -1){
+                inc += dp[get];
+                // dp[i] = inc;
+            }
+            int exc = dp[i-1];
+            // else{
+                dp[i] = max(inc,exc);
+            // }
+        }
+        return dp[n-1];
+    }
+};
