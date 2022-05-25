b5868b9c84203f8c84c429361e6880d8520e0755
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-25 20:23:20 +0530

    Time: 35 ms (41.82%), Space: 13.4 MB (79.85%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(vector<vector<int>>& graph,int par,int src, int col, vector<int>&vis){
+        for(auto i:graph[src]){
+            if(vis[i] == -1){
+                vis[i] = 3 - vis[src];
+                if(!solve(graph, src, i, 3-col, vis)){
+                    return false;
+                };
+            }
+            else if(i!=par && vis[src] == vis[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>vis(n,-1);
+        for(int i = 0;i<n;i++){
+            if(vis[i]==-1){
+                vis[i] = 1;
+                if(!solve(graph, i, i, 1, vis)){
+                    return false;
+                }   
+            }
+        }
+        return true;
+    }
+};
