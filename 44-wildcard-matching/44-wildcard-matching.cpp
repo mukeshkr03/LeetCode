10ff9896403941844bac9e9eea18b5459f335149
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-05-26 21:46:41 +0530

    Time: 96 ms (41.26%), Space: 27.3 MB (27.54%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -4,22 +4,26 @@ public:
      int n=p.size(); //having * and ? character
 int m=s.size();
 
-   vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+   vector<vector<int>> dp(n+1,vector<int>(m+1,0));
     for(int j=0;j<=m;j++){
-        dp[0][j]=false;
+        dp[0][j]=0;
     }
-    bool check=true;
+    int check=1;
     for(int i=1;i<=n;i++){
-        if(p[i-1]!='*') check=false;
+        if(p[i-1]=='*')
         dp[i][0]=check;
+        else{
+            check = 0;
+        }
     }
-    dp[0][0]=true;
+    // dp[0][0]=true;
+    dp[0][0]=1;
     check=true;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(p[i-1]==s[j-1] or p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
             else if(p[i-1]=='*') dp[i][j]=dp[i-1][j] or dp[i][j-1];
-            else dp[i][j]=false;
+            else dp[i][j]=0;
         }
     }
     //     for(int i=0;i<=n;i++){
