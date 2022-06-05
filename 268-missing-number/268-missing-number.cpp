2592c3371ef396287a1971905ee8f842da21247e
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-05 09:57:15 +0530

    Time: 55 ms (5.63%), Space: 17.8 MB (93.52%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int temp = n*(n+1)/2;
+        int temp = (n*(n+1))/2;
         // cout<<n<<endl;
         // cout<<temp<<endl;
         for(int i = 0;i<n;i++){
