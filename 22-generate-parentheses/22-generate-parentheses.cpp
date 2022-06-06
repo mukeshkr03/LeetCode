8e33d82bbd27def110280105103e529322a8da4b
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-06 09:53:24 +0530

    Time: 8 ms (41.17%), Space: 15.7 MB (23.46%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string>ans;
+        void dfs(int l, int r, string s){
+            if(l==0 && r==0){
+                ans.push_back(s);
+                return;
+            }
+            
+            if(l>0){
+                s.push_back('(');
+                dfs(l-1,r,s);
+                s.pop_back();
+            }
+             if(r>l && r>0){
+                s.push_back(')');
+                dfs(l,r-1,s);
+            }
+        }
+    vector<string> generateParenthesis(int n) {
+        // string s;
+        dfs(n,n,"");
+        return ans;
+    }
+};
