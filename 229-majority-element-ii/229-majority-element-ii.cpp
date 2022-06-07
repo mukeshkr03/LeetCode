60179796a9228a32e38a96eb599a4d0ab8ab3c1b
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-07 23:34:19 +0530

    Time: 32 ms (6.96%), Space: 15.8 MB (52.51%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,17 +1,55 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int,int>m;
-        int n = nums.size();
-        for(int i = 0;i<n;i++){
-            m[nums[i]]++;
-        }
+        int index1 = INT_MIN,index2 = INT_MIN, cnt1 = 0,cnt2 = 0, n = nums.size();
         vector<int>ans;
-        for(auto i:m){
-            if(i.second > n/3){
-                ans.push_back(i.first);
+        for(int i = 0;i<n;i++){
+            if(nums[i] ==  index1){
+                cnt1++;
+            }
+            else if(nums[i] == index2){
+                cnt2++;
+            }
+            else if(cnt1 == 0){
+                index1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0){
+                index2 = nums[i];
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        return ans;
-    }
+            // if(cnt == 0){
+            //     index = i;
+            //     cnt = 1;
+            // }
+            int ans1 = 0,ans2 = 0;
+            // if(cnt1 >0){
+                for(int i = 0;i<n;i++){
+                    if(nums[i] == index1){
+                        ans1++;
+                    }
+                }
+            // }
+            if(ans1>n/3){
+                ans.push_back(index1);
+            }
+            
+            // if(cnt2 >0){
+                for(int i = 0;i<n;i++){
+                    if(nums[i] == index2){
+                        ans2++;
+                    }
+                }
+            // }
+            if(ans2>n/3){
+                ans.push_back(index2);
+            }
+            return ans;  
+        }
+        // cout<<nums[index]<<endl 
 };
