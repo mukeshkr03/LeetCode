e2baf896a68422ca3cf39311d238e2410c85c8bb
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-08 01:03:28 +0530

    Time: 215 ms (5.13%), Space: 44 MB (15.94%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
        map<int,int>m;
-        // m[0] = 1;
+        m[0] = 1;
         int sum = 0,ans = 0;
         for(int i = 0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum == k){
-                ans++;
-            }
             int x = (sum-k);
            if(m.count(x)){
                 ans += m[x];
