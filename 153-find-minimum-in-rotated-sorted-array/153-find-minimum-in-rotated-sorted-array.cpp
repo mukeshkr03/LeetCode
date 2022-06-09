2b7a7929881b10448ed8f547324aeae94f6797e4
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-09 09:37:21 +0530

    Time: 7 ms (37.34%), Space: 10.3 MB (23.63%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -14,14 +14,14 @@ public:
             if(nums[mid] > nums[mid + 1]){
                 return nums[mid+1];
             }
-            if(nums[mid] < nums[mid + 1] && nums[mid] < nums[mid - 1]){
-                return nums[mid];
-            }
+            // if(nums[mid] < nums[mid + 1] && nums[mid] < nums[mid - 1]){
+            //     return nums[mid];
+            // }
             if(nums[mid] > nums[s]){
                 s = mid + 1;
             }
             else{
-                e = mid - 1;
+                e = mid;
             }
         }
         return ans;
