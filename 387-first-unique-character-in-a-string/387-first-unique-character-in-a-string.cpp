4b5c7d8124f7505163257984a0fdf443f0fc6dee
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-10 11:07:45 +0530

    Time: 24 ms (87.95%), Space: 10.6 MB (71.40%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int frq[26]={0};
         int index=-1;
         for(int i=0;i<s.length();i++){
-            frq[s[i]-'a']++;
+            frq[s[i] - 'a']++;
         }
         for(int i=0;i<s.length();i++){
            if(frq[s[i] - 'a'] == 1){
