71ea199eec294541ddb0d4475858a3f6ea834453
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-13 12:18:54 +0530

    Time: 828 ms (55.36%), Space: 97.8 MB (87.27%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.cpp
+++ 827-making-a-large-island/827-making-a-large-island.cpp
@@ -12,60 +12,61 @@ public:
         if(j<n-1) findAIsland(grid,i,j+1,ok,mp,cnt);
 
     }
+    
     int largestIsland(vector<vector<int>>& grid) {
-         int n = grid.size(), m = grid[0].size(),ok = 1;
+        int n = grid.size(), m = grid[0].size();
         map<int,int>mp;
+        
+        int ok = 1;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j]==1){
-                    int cnt = 0;
+                if(grid[i][j] == 1){
                     ok++;
+                    int cnt = 0;
                     findAIsland(grid,i,j,ok,mp,cnt);
                     mp[ok] = cnt;
                 }
             }
         }
-
-        int ans = INT_MIN;
-
+        
+       
+        int ans = INT_MIN; 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j]==0){
-                    set<int>s;
-                   if(i>0 && grid[i-1][j]>1){
+                if(grid[i][j] == 0){
+                     set<int>s;
+                     if(i>0 && grid[i-1][j]>1){
                         if(s.count(grid[i-1][j])==0){
                             s.insert(grid[i-1][j]);
                         }
                     }
-                    if(j>0 && grid[i][j-1]>1){
+                     if(j>0 && grid[i][j-1]>1){
                         if(s.count(grid[i][j-1])==0){
                             s.insert(grid[i][j-1]);
                         }
                     }
                     if(i<n-1 && grid[i+1][j]>1){
-                        if(s.count(grid[i+1][j])==0){
-                            s.insert(grid[i+1][j]);
-                        }
+                            if(s.count(grid[i+1][j])==0){
+                                s.insert(grid[i+1][j]);
+                            }
                     }
                     if(j<n-1 && grid[i][j+1]>1){
-                        if(s.count(grid[i][j+1])==0){
-                            s.insert(grid[i][j+1]);
-                        }
+                            if(s.count(grid[i][j+1])==0){
+                                s.insert(grid[i][j+1]);
+                            }
                     }
-
                     int sol = 0;
                     for(auto i:s){
                         sol+=mp[i];
                     }
                     ans = max(ans,sol+1);
                     s.clear();
-                }
-            }
-        }
-        if(ans==INT_MIN){
+            }   
+        }       
+    }
+        if(ans == INT_MIN){
             return n*n;
-        }
+        } 
         return ans;
-
     }
 };
