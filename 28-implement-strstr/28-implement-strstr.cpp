f7abac95f07b784dd28fbede624bac69accf5f3f
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-14 13:51:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (90.06%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        char x = needle[0];
-        string ans;
-        vector<pair<string,int>>sol;
-        for(int i = 0;i<haystack.length();i++){
-            if(x==haystack[i]){
-                string ans = haystack.substr(i,needle.length());
-                sol.push_back({ans,i});
-                ans = "";
-            }
-        }
-        for(auto i:sol){
-            // cout<<i.first<<" ";
-            if(i.first == needle){
-                return i.second;
-            }
-        }
-        // cout<<endl;
-        return -1;
+        return haystack.find(needle);
     }
 };
