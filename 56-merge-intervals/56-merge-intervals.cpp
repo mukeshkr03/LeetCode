45f5014115bba9d87fcd0fedca713eb383ea5532
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-15 12:21:11 +0530

    Time: 87 ms (15.99%), Space: 19.4 MB (43.94%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
-    
-    static int compare(vector<int>a, vector<int>b){
-        return a[1]>b[1];
-    }
     vector<vector<int>> merge(vector<vector<int>>& I) { 
         sort(I.begin(),I.end());
-        for(auto i:I){
-            cout<<i[0]<<" "<<i[1]<<endl;
-        }
         vector<int>v;
         int start = I[0][0];
         int end = I[0][1];
@@ -19,7 +12,7 @@ public:
                 continue;
             }
             else if(I[i][0]<=end){
-                end = max(end,I[i][1]);
+                end = I[i][1];
             }
             else{
                 v.push_back(start);
