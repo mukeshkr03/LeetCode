5abdc47f2af432bd75bb7fe05a898254ec3795d4
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-15 22:58:50 +0530

    Time: 24 ms (25.80%), Space: 12.8 MB (16.48%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,35 @@
+bool compFunc(const pair<int, string>& p1, const pair<int, string>& p2)
+{
+    if (p1.first > p2.first)
+        return true;
+    
+    if (p1.first == p2.first)
+        return p1.second < p2.second;
+    
+    return false;
+}
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> ht;
+        for (auto& w : words)
+        {
+            ht[w]++;
+        }
+        
+        vector<pair<int, string>> vec;
+        for (auto& [w, c] : ht)
+        {
+            vec.push_back({c,w});
+        }
+        sort(vec.begin(), vec.end(), &compFunc);
+        
+        vector<string> ret(k);
+        for (int i = 0; i< k; i++)
+        {
+            ret[i] = vec[i].second;
+        }
+        
+        return ret;
+    }
+};
