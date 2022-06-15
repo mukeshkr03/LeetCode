313fd1c95453fdf11a4e7c9a96ad8e15394f3ed2
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-15 22:59:25 +0530

    Time: 18 ms (52.98%), Space: 12.7 MB (35.76%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -4,7 +4,7 @@ bool compFunc(const pair<int, string>& p1, const pair<int, string>& p2)
         return true;
     
     if (p1.first == p2.first)
-        return p1.second.compare(p2.second) < 0;
+        return p1.second < p2.second;
     
     return false;
 }
