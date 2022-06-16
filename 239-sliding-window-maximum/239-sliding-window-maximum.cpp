77d59db2af6ee7a6e375e7037d7905593c2f665b
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-16 19:26:35 +0530

    Time: 1006 ms (5.60%), Space: 194.1 MB (10.58%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       
-       vector<int>ans;
-    int i = 0,j = 0;
-    list<int>l;
-    while(j<nums.size()){
-        while(nums[j]>l.back() && !l.empty()){
-            l.pop_back();
-        }
-        l.push_back(nums[j]);
-
-        if(j-i+1<k){
-            // l.pb(nums[j]);
-            j++;
-        }
-        else{
-            if(j-i+1==k){
-                ans.push_back(l.front());
-                if(l.front()==nums[i]){
-                    l.pop_front();
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i = 0,j = 0;
+        vector<int>ans;
+        list<int>l;
+        while(j<nums.size()){
+            while(!l.empty() && l.back()<nums[j]){
+                l.pop_back();
+            }
+            l.push_back(nums[j]);
+            
+            if(j-i+1 < k){
+                j++;
+            }
+            else{
+                if(j-i+1 == k){
+                    ans.push_back(l.front());
+                    if(l.front() == nums[i]){
+                        l.pop_front();
+                    }
+                    j++;
+                    i++;
                 }
-                i++;j++;
             }
         }
-    }
-       return ans;
-        
+        return ans;
     }
 };
+
