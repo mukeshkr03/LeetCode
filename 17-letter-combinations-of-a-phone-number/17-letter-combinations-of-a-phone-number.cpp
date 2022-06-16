e4dc87baa0373d12f208af5d436ae74e2ab5cce3
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-16 17:32:46 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (34.96%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string d) {
+        string a[10] = {"","","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        
+        if(d.size()==0) return {};
+        
+            vector<string>ans;
+            ans.push_back("");
+        
+            for(char c : d){
+                vector<string>temp;
+                for(char k : a[c-'0']){
+                    for(string x: ans){
+                        temp.push_back(x+k);
+                    }            
+                }
+                ans = temp;
+            }
+        return ans;
+    }
+};
