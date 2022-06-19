7b36629eee763d7197333b68a8afb9c2cadb4957
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-19 23:34:14 +0530

    Time: 14 ms (11.58%), Space: 9.9 MB (6.67%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -10,16 +10,10 @@ public:
         for(int i = 0;i<words.size();i++){
             string ans = "";
             for(int j = 0;j<words[i].length();j++){
-                // i[j] = mp[i[j]];
                 ans+=mp[words[i][j]];
             }
             words[i] = ans;
         }
-        
-        // for(auto i:mp){
-        //     cout<<i.first<<" "<<i.second<<endl;
-        // }
-        // cout<<endl;
         
         for(int i = 0;i<n-1;i++){
             if(words[i]>words[i+1]){
