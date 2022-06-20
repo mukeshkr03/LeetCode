940b7e13e94fd919a0201f239f105295cc231247
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-06-20 13:47:06 +0530

    Time: 57 ms (52.48%), Space: 89.8 MB (25.13%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -6,18 +6,11 @@ public:
         }
         
         sort(nums1.begin(), nums1.end());
-        
-        // for(auto i: nums1){
-        //     cout<<i<<" ";
-        // }
-        // return 1.300000;
         double ans = nums1[(nums1.size())/2]; 
         if(nums1.size() % 2 == 0){
             double x = nums1[(nums1.size())/2], y = nums1[(nums1.size())/2 - 1]; 
             ans = (x+y)/2;
             double z = (x+y)/2;
-            cout<<z<<endl;
-            cout<<x<<" "<<y<<endl;
         }
         return (ans);
     }
