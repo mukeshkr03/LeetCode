cb63c9c10d6a71cd37cf6141f2f09de1eeb0f6d1
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-07-26 19:28:12 +0530

    Time: 2573 ms (5.02%), Space: 11.5 MB (96.64%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int a[n];
+        for(int i = 0;i<n;i++){
+            a[i] = 0;
+        }
+        a[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            int ok = 0;
+            for(int j = i-1;j>=0;j--){
+                if(nums[i] != nums[j]+1){
+                    ok = max(ok,a[j]);
+                    // break;
+                }
+            }
+            if(ok){
+                a[i] = ok + nums[i];
+            }
+            else if(ok == 0){
+                a[i] = nums[i];
+            }
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            ans = max(ans,a[i]);
+        }
+        // for(auto i:a){
+        //     cout<<i<<" ";
+        // }
+        // cout<<endl;
+        return ans;
+    }
+};
+
+// 2,2,3,3,3,4
+// 2 4 3 6 9  
+    
+//     2 3 4
+//     2 3 6
