9039f4d283a78c7b193508a2749656f6432f259f
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-08-03 10:07:28 +0530

    Time: 4 ms (35.96%), Space: 6.3 MB (95.75%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string ans(string &s, int &i){
+        string res;
+        while(i<s.length() && s[i]!=']'){
+            if(isdigit(s[i])){
+                int k = 0;
+                while(isdigit(s[i]) && i<s.length()){
+                    k = k*10 + s[i] - '0';
+                    i++;
+                }
+                i++;
+                string r = ans(s,i);
+                while(k>0){
+                    res+=r;
+                    k--;
+                }
+                i++;
+            }
+            else{
+                res+=s[i];i++;
+            }
+        }
+        return res;
+    }
+    string decodeString(string s) {
+        int i = 0;
+        return ans(s,i);
+    }
+};
