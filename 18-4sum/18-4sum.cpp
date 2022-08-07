73c237a3ead6cef4cd6327919aedaf77c23d3506
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-08-08 00:30:45 +0530

    Time: 1683 ms (5.00%), Space: 78.6 MB (8.35%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>>s;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                for(int k = j+1;k<n;k++){
+                   long long x = (long long)target - (long long)nums[i]- (long long)nums[j] - (long long)nums[k]; 
+                   
+                    if(binary_search(nums.begin()+k+1,nums.end(),x))
+                          s.insert(vector<int>{nums[i],nums[j],nums[k],(int)x}); 
+                        
+                    }
+                }
+            }
+        
+     vector<vector<int>>ans(s.begin(),s.end());
+        return ans;
+    }
+};
