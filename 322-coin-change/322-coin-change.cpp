b9c00e6c7fee36489045e5a0f2bf70fe982cdf2e
Mukesh Kumar Das <77975741+mukeshkr03@users.noreply.github.com>
2022-09-19 16:51:22 +0530

    Time: 142 ms (56.23%), Space: 10.6 MB (88.89%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -9,16 +9,16 @@ public:
         for(int i = 0;i<=amount;i++){
             a[0][i] = INT_MAX - 1;
         }
-        for(int i = 1;i<=amount;i++){
-            if(i%coins[0] == 0){
-                a[1][i] = i/coins[0];
-            }
-            else{
-                a[1][i] = INT_MAX - 1;
-            }
-        }
+//         for(int i = 1;i<=amount;i++){
+//             if(i%coins[0] == 0){
+//                 a[1][i] = i/coins[0];
+//             }
+//             else{
+//                 a[1][i] = INT_MAX - 1;
+//             }
+//         }
         
-        for(int i = 2;i<=n;i++){
+        for(int i = 1;i<=n;i++){
             for(int j = 1;j<=amount;j++){
                 if(coins[i-1] <= j){
                     a[i][j] = min(1 + a[i][j - coins[i-1]], a[i-1][j]);
